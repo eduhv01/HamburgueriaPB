f9484b086cd1d27b9b8140dad5126aa104540df5
eduhv01 <eduardohv1021@gmail.com>
2024-09-25 17:00:43 -0300

    atualização de tela com css e js.

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,4 +1,19 @@
 window.addEventListener("load", function () {
     const preloader = document.getElementById("preloader");
     preloader.classList.add("hidden");
+
+    document.getElementById('formContato').addEventListener('submit', function (event) {
+        event.preventDefault(); 
+
+        const mensagemDeAgradecimento = document.getElementById('mensagemDeAgradecimento');
+
+        mensagemDeAgradecimento.style.display = 'block';
+        mensagemDeAgradecimento.textContent = 'Obrigado por sua avaliação :)';
+
+        this.reset();
+
+        setTimeout(function () {
+            mensagemDeAgradecimento.style.display = 'none';
+        }, 3000);
+    });
 });
